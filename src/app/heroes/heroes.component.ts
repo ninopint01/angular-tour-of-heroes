5327afd47a8bf9473c60b3c2270fc533d73b87b9
ninopint01 <100156122+ninopint01@users.noreply.github.com>
2022-05-24 22:27:33 +0200

    FINISHED Add Navigation

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -14,18 +14,18 @@ export class HeroesComponent implements OnInit {
   
   // heroes = HEROES;
   heroes: Hero[] = [];
-  selectedHero?: Hero;
+  // selectedHero?: Hero;
 
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  constructor(private heroService: HeroService/*, private messageService: MessageService*/) { }
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
+  // onSelect(hero: Hero): void {
+  //   this.selectedHero = hero;
+  //   this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  // }
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
